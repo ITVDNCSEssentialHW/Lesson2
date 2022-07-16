3c23bb7082526df349021af5085a69c62fdbb8a5
Никита Бочаров <bochanikita@yandex.ru>
2022-07-16 13:35:50 +0300

    Добавьте файлы проекта.

---
+++ Task 2/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    class Converter
+    {
+        double usd, eur, rub, grv, currencyVol;
+        bool sell;
+
+        public Converter(double usd, double eur, double rub, double grv)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+            this.grv = grv;
+        }
+
+        private double GRVtoUSD()
+        {
+            return grv / usd;
+        }
+        private double GRVtoEUR()
+        {
+            return grv / eur;
+        }
+        private double GRVtoRUB()
+        {
+            return grv / rub;
+        }
+        
+        public Converter(double usd, double eur, double rub, double currencyVol, bool sell)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+            this.currencyVol = currencyVol;
+            this.sell = true;
+        }
+
+        private double USDtoGRV()
+        {
+            return currencyVol * usd;
+        }
+        private double EURtoGRV()
+        {
+            return currencyVol * eur;
+        }
+        private double RUBtoGRV()
+        {
+            return currencyVol * rub;
+        }
+        //Buy
+        public double GRVUSD { get { return GRVtoUSD(); } }
+        public double GRVEUR { get { return GRVtoEUR(); } }
+        public double GRVRUB { get { return GRVtoRUB(); } }
+        //Sell
+        public double USDGRV { get { return USDtoGRV(); } }
+        public double EURGRV { get { return EURtoGRV(); } }
+        public double RUBGRV { get { return RUBtoGRV(); } }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grv = 850;
+            Converter conv = new Converter(77.2, 85.4, 34.3, grv);
+
+            double usd = conv.GRVUSD;
+            Console.WriteLine(usd);
+
+            Console.ReadLine();
+        }
+    }
+}
